66cae3e9addf2324af5f035df12382742329c120
Adrian Lewis <adrian.lewis@immediate.co.uk>
2019-01-14 17:48:11 +0000

    Fixed build chain to generate our nuget packages.

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@ choco install gitversion.portable --pre --y
 choco upgrade gitversion.portable --pre --y
 
 # Display minimal restore information
-dotnet restore ./src/NEventStore.Core.sln --verbosity m
+dotnet restore ./src/NEventStoreAsync.Core.sln --verbosity m
 
 # GitVersion 
 $str = gitversion /updateAssemblyInfo | out-string
@@ -21,16 +21,16 @@ $nugetversion = $json.NuGetVersion
 
 # Build
 Write-Host "Building: "$nugetversion
-dotnet build ./src/NEventStore.Core.sln -c $configuration --no-restore
+dotnet build ./src/NEventStoreAsync.Core.sln -c $configuration --no-restore
 
 # Testing
 if ($runtests -eq "y") {
     Write-Host "Executing Tests"
-    dotnet test ./src/NEventStore.Core.sln -c $configuration --no-build
+    dotnet test ./src/NEventStoreAsync.Core.sln -c $configuration --no-build
     Write-Host "Tests Execution Complated"
 }
 
 # NuGet packages
 Write-Host "NuGet Packages creation"
-dotnet pack ./src/NEventStore/NEventStore.Core.csproj -c $configuration --no-build -o $artifacts /p:PackageVersion=$nugetversion
-dotnet pack ./src/NEventStore.Serialization.Json/NEventStore.Serialization.Json.Core.csproj -c $configuration --no-build -o $artifacts /p:PackageVersion=$nugetversion
+dotnet pack ./src/NEventStore/NEventStoreAsync.Core.csproj -c $configuration --no-build -o $artifacts /p:PackageVersion=$nugetversion
+dotnet pack ./src/NEventStore.Serialization.Json/NEventStoreAsync.Serialization.Json.Core.csproj -c $configuration --no-build -o $artifacts /p:PackageVersion=$nugetversion
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,6 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("6.0.0.0")]
 [assembly: AssemblyFileVersion("6.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0+74.Branch.master.Sha.b1ce2c493558d56de8511955a5a1a9d1182788c1")]

8a3f89fc4bc74e36bad6511b6d5731a99f1ea6d9
Adrian Lewis <zen_global@hotmail.com>
2019-01-16 22:10:28 +0000

    Built release : 1.0.0-alpha7

--- src/NEventStore.Serialization.Json/Properties/AssemblyInfo.cs
+++ src/NEventStore.Serialization.Json/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.4+Branch.master.Sha.75615b8191986d1df98ef6446e70b5f05a88c0e6")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.7+Branch.master.Sha.f470ac5dfda8f2f1f3ce4c917a23f80a890bd3ec")]
